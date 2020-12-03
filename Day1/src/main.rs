b633ca925a3f4bdb8153aa68fd451596a0b4cc68
Magnus Karlsson <Magnus.Karlsson@diadrom.se>
2020-12-03 22:52:51 +0100

    Day1 part 2

--- Day1/src/main.rs
+++ Day1/src/main.rs
@@ -13,8 +13,23 @@ fn get_product() -> u64{
     0
 }
 
+fn get_product_part2() -> u64{
+    let count = EXPENSES.len();
+    for i in 0..count {
+        for k in (i + 1)..count {
+            for l in (k+1)..count{
+                if EXPENSES[i] + EXPENSES[k]+ EXPENSES[l] == 2020 {
+                    return EXPENSES[i] * EXPENSES[k] * EXPENSES[l];
+                }
+            }
+        }
+    };
+    0
+}
+
 fn main() {
 
 
-    println!("{}",get_product());
+    println!("part1 {}",get_product());
+    println!("part2 {}",get_product_part2());
 }
