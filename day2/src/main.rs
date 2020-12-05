374c433312adf8cbfec4abf73249b0d533e4eba8
SweTailor94 <magnus.l.karlsson@gmail.com>
2020-12-05 01:50:46 +0100

    Part 1 and 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,11 +1,21 @@
-use crate::password_list::{get_passwords_w_rules, PasswordRuleAndValue};
+use crate::password_list::{get_passwords_w_rules};
+use std::env::current_dir;
 
 mod password_list;
 
 fn main() {
-    let mut valid_passwords= get_passwords_w_rules("password.txt").into_iter().
+    let passwords=
+        get_passwords_w_rules("src/passwords.txt");
+
+    let valid_passwords = passwords.into_iter().
         filter(|x|x.is_valid()) ;
 
 
-    println!("There are {} valid passwords",valid_passwords.count());
+
+    println!("There are {} valid passwords in part 1",valid_passwords.count());
+
+    let passwords = get_passwords_w_rules("src/passwords.txt");
+    let valid_passwords_part2  = passwords.into_iter().
+        filter(|x|x.is_valid_part2()) ;
+    println!("There are {} valid passwords in part 2",valid_passwords_part2.count());
 }
--- day2/src/password_list.rs
+++ day2/src/password_list.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
 
 
 /// Parse the input file into a nice struct
@@ -16,16 +18,37 @@ impl PasswordRuleAndValue{
         let chars:Vec<&str> = self.password.matches(&*self.letter).collect();
         return chars.len() as i32 >= self.min && chars.len() as i32 <= self.max ;
     }
+    
+    pub fn is_valid_part2(&self) -> bool{
+        let pwd = self.password.as_bytes();
+        let char = self.letter.as_bytes();
 
-    pub fn new(&mut self, value:&str){
-        let s : Vec<&str> = value.split(":").collect();
-        let rule = s[0];
-        self.password = s[1].trim().to_string();
+        pwd[self.min as usize - 1] != pwd[self.max as usize -1] &&
+            (pwd[self.min as usize - 1] == char[0] || pwd[self.max as usize -1] == char[0] )
+    }
+
+    pub fn new( value:&str) -> PasswordRuleAndValue{
+        let rule_pwd : Vec<&str> = value.split(":").collect();
+        let limits_letter : Vec<&str> = rule_pwd[0].split(" ").collect();
+        let min_max : Vec<&str>= limits_letter[0].split("-").collect();
+
+        PasswordRuleAndValue {
+            min : min_max[0].parse::<i32>().unwrap(),
+            max : min_max[1].parse::<i32>().unwrap(),
+            letter : limits_letter[1].trim().to_string(),
+            password : rule_pwd[1].trim().to_string()
+        }
     }
 }
 
 pub fn get_passwords_w_rules(file_name:&str) -> Vec<PasswordRuleAndValue> {
-    let  all_of_them : Vec<PasswordRuleAndValue> = Vec::new();
+    let mut all_of_them : Vec<PasswordRuleAndValue> = Vec::new();
+    let file = File::open(file_name).unwrap();
+    let reader = BufReader::new(file);
 
+    for line in reader.lines() {
+        let val = PasswordRuleAndValue::new(line.unwrap().as_str());
+        all_of_them.push(val);
+    }
     return all_of_them;
 }
